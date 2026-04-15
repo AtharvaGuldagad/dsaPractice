c585531f2609965ed086448f50bdddbf829bbc9e
AtharvaGuldagad <atharvaguldagad@gmail.com>
2026-04-15 23:59:23 +0530

    solved paranthesis

---
+++ ValidParentheses/Solution.java
@@ -0,0 +1,21 @@
+package ValidParentheses;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack <Character>S=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='('||c=='['||c=='{'){
+                S.push(c);
+            } else {
+                if(S.isEmpty()) return false;
+                char top=S.pop();
+                if(c==')'&&top!='('||c==']'&&top!='['||c=='}'&&top!='{'){
+                    return false;
+                }
+            }
+        }
+        return S.isEmpty();
+    }
+}
