2c8b72c19d44628e0694c7872e6c232bfbee3cf1
Your Actual Name <atharvaguldagad@gmail.com>
2026-07-31 23:06:28 +0530

    Solved

---
+++ RevBits/Solution.java
@@ -0,0 +1,12 @@
+package RevBits;
+
+public class Solution {
+    public int reverseBits(int n) {
+       int res=0;
+       for(int i=0;i<32;i++){
+        int b=(n>>i)&1;
+        res=res+(b<<(31-i));
+       } 
+       return res;
+    }
+}
