c444d1646eda0f116f6d3234c40908a24fe09a72
Your Actual Name <atharvaguldagad@gmail.com>
2026-08-05 21:51:43 +0530

    Solved

---
+++ RevInteger/Solution.java
@@ -0,0 +1,18 @@
+package RevInteger;
+
+public class Solution {
+    public int reverse(int x) {
+        int min=-2147483648;
+        int max=2147483647;
+        int res=0;
+        while(x!=0){
+            int digit=x%10;
+            x=x/10;
+            if(res>max/10||(res==max/10&&digit>max%10)) return 0;
+            if(res<min/10||(res==min/10&&digit<min%10)) return 0;
+            res=(res*10)+digit;
+        }
+        return res;
+    }
+}
+
