1352c22f9c828403d162975aeffe3a0e2d1b9c91
Your Actual Name <atharvaguldagad@gmail.com>
2026-08-11 19:34:18 +0530

    SOlved

---
+++ BtreeRightHandView/Solution.java
@@ -0,0 +1,34 @@
+package BtreeRightHandView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+public class Solution {
+    List<Integer> res=new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        dfs(root,0);
+        return res;
+    }
+    private void dfs(TreeNode node, int depth){
+        if(node==null) return;
+        if(res.size()==depth) res.add(node.val);
+        dfs(node.right,depth+1);
+        dfs(node.left,depth+1);
+    }
+}
+
