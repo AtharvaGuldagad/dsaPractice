4ea1be1f4c79f7cad36f3d1742e3278214958265
Your Actual Name <atharvaguldagad@gmail.com>
2026-08-13 21:03:23 +0530

    Solved

---
+++ KSmallestInBST/Solution.java
@@ -0,0 +1,37 @@
+package KSmallestInBST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+public class Solution {
+    List<Integer> ar=new ArrayList<>();
+    public int kthSmallest(TreeNode root, int k) {       
+        dfs(root);
+        return ar.get(k-1);
+
+    }
+    void dfs(TreeNode node){
+            if(node==null) return;
+
+            dfs(node.left);
+            ar.add(node.val);
+            dfs(node.right);
+        }
+
+}
+
