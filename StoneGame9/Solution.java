141912cfea144f3f5566970af8b42495ef313fab
Your Actual Name <atharvaguldagad@gmail.com>
2026-08-16 10:24:04 +0530

    Solved

---
+++ StoneGame9/Solution.java
@@ -0,0 +1,20 @@
+package StoneGame9;
+
+public class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int c0=0;
+        int c1=0;
+        int c2=0;
+        for(int i:stones){
+            if(i%3==0){
+                c0++;
+            }else if(i%3==1){
+                c1++;
+            } else{
+                c2++;
+            }
+        }
+        if(c0%2==0) return c1>0 && c2>0;
+        return Math.abs(c1-c2)>2;
+    }
+}
