b20274a19242160ed2966108d18d3c4d58f93388
Your Actual Name <atharvaguldagad@gmail.com>
2026-08-25 19:30:42 +0530

    Refactor

--- 3Sum/Solution.java
+++ 3Sum/Solution.java
@@ -7,9 +7,7 @@ public class Solution {
         Arrays.sort(nums);
         List<List<Integer>> sol = new ArrayList<>();
         for (int i=0;i<nums.length-2;i++) {
-            if (i>0&&nums[i]==nums[i-1]) {
-                continue;
-            }
+            if (i>0&&nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=nums.length-1;
             while(j<k) {
