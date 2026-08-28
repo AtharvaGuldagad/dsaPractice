743ce90993959cb7d68f38d208d34a641cbc1592
Your Actual Name <atharvaguldagad@gmail.com>
2026-08-29 00:49:13 +0530

    Solved

---
+++ KthLargestEleInStream/Solution.java
@@ -0,0 +1,25 @@
+package KthLargestEleInStream;
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    class KthLargest {
+    PriorityQueue<Integer> minHeap;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        this.minHeap=new PriorityQueue<>();
+        for(int num:nums){
+            minHeap.offer(num);
+            if(minHeap.size()>k) minHeap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        minHeap.offer(val);
+        if(minHeap.size()>k) minHeap.poll();
+        return minHeap.peek();
+    }
+}
+
+}
