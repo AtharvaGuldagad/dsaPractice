84e4b9c593a3636c959a08928e6816640cf4609c
Your Actual Name <atharvaguldagad@gmail.com>
2026-08-31 21:25:37 +0530

    Solved

---
+++ NoOfSeniorCitizen/Solution.java
@@ -0,0 +1,11 @@
+package NoOfSeniorCitizen;
+
+public class Solution {
+    public int countSeniors(String[] details) {
+        int res=0;
+        for(String d: details){
+            if(Integer.parseInt(d.substring(11,13))>60) res++;
+        }
+        return res;
+    }
+}
