689529c1e10d98433da58f8cbdc56515466ceb70
Your Actual Name <atharvaguldagad@gmail.com>
2026-09-01 18:19:16 +0530

    Solved

---
+++ LargestEletoRight/Solution.java
@@ -0,0 +1,16 @@
+package LargestEletoRight;
+
+public class Solution {
+    public int[] replaceElements(int[] arr) {
+        int temp=0;
+        for(int i=0;i<arr.length;i++){
+            int max=-1;
+            for(int j=i+1;j<arr.length;j++){
+                max=Math.max(max,arr[j]);
+            }
+            arr[i]=max;
+        }
+        arr[arr.length-1]=-1;
+        return arr;
+    }
+}
