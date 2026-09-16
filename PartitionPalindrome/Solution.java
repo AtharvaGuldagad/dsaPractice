2de5dcc3c92dbdebdd31272f481c2db2ae36c0ed
Your Actual Name <atharvaguldagad@gmail.com>
2026-09-16 22:54:40 +0530

    Solved

---
+++ PartitionPalindrome/Solution.java
@@ -0,0 +1,36 @@
+package PartitionPalindrome;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res=new ArrayList<>();
+        List<String> part= new ArrayList<>();
+        dfs(0,s,part,res);
+        return res;
+    }
+    void dfs(int i, String s, List<String> part, List<List<String>> res){
+        if(i>=s.length()){
+            res.add(new ArrayList<>(part));
+            return;
+        }
+        for(int j=i;j<s.length();j++){
+            if(isPalin(s,i,j)){
+                part.add(s.substring(i,j+1));
+                dfs(j+1,s,part,res);
+                part.remove(part.size()-1);
+            }
+        }
+    }
+
+    boolean isPalin(String s, int l, int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
+
